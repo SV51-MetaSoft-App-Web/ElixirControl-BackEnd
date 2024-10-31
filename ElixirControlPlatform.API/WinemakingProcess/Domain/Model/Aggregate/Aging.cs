580a8a297d384ae6221dadabfc88c6eeeee01fb3
Janover Saldaña <u20201b510@upc.edu.pe>
2024-10-30 23:55:35 -0500

    feat(winemaking-process): added Aging aggregates.

---
+++ ElixirControlPlatform.API/WinemakingProcess/Domain/Model/Aggregate/Aging.cs
@@ -0,0 +1,30 @@
+﻿namespace ElixirControlPlatform.API.WinemakingProcess.Domain.Model.Aggregate;
+
+public class Aging
+{
+    public int Id { get; private set; }
+    public int BatchId { get; private set; }
+    
+    public string BarrelType { get; private set; }
+    public int AgingDurationMonths { get; private set; }
+    public int InspectionsPerformed { get; private set; }
+    public string InspectionsResult { get; private set; }
+    
+    public string StartDate   { get; private set; }
+    public string EndDate { get; private set; }
+
+    protected Aging()
+    {
+        this.BarrelType = string.Empty;
+        this.AgingDurationMonths = 0;
+        this.InspectionsPerformed = 0;
+        this.InspectionsResult = string.Empty;
+        this.StartDate = string.Empty;
+        this.EndDate = string.Empty;
+    }
+    
+    
+    
+}
+
+
