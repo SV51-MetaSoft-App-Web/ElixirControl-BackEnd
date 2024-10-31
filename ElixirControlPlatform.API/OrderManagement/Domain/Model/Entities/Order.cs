730d4b54bedb30f40212a956c86646683542d5c1
vquijandria <u201822697@upc.edu.pe>
2024-10-30 23:34:50 -0500

    feat(Order): created the order entity on the domain

---
+++ ElixirControlPlatform.API/OrderManagement/Domain/Model/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace ElixirControlPlatform.API.OrderManagement.Domain.Model.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public int Quantity { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+        public string ContactName { get; set; }
+        public string ProductName { get; set; }
+        public string TransportCondition { get; set; }
+        public string PaymentTerms { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Ruc { get; set; }
+        public string WineType { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+    
+    public Order() {
+    
+    }
+}
