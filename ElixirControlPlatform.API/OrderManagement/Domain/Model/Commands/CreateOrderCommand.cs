b6a4487d39f310cb37fa4b5fb73de400fb853226
vquijandria <u201822697@upc.edu.pe>
2024-10-30 19:31:28 -0500

    feat(CreateOrderCommand): added the command for Create Order

---
+++ ElixirControlPlatform.API/OrderManagement/Domain/Model/Commands/CreateOrderCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public record CreateOrderCommand(int Id, string BusinessName, string RequestedDate, int Quantity, string Phone, string Status, string ContactName, string ProductName, string TransportCondition, string PaymentTerms, string Address, string Email, string Ruc, string WineType, string PaymentMethod, string DeliveryDate);
