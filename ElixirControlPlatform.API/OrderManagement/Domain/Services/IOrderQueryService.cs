ccc505d2c222a0f9635996b19c1bdbdd7af763ab
vquijandria <u201822697@upc.edu.pe>
2024-10-30 22:38:22 -0500

    feat(IOrderQueryService): added the Order Query Service interface

---
+++ ElixirControlPlatform.API/OrderManagement/Domain/Services/IOrderQueryService.cs
@@ -0,0 +1,18 @@
+﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Queries;
+using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
+
+namespace ElixirControlPlatform.API.OrderManagement.Domain.Services;
+/// <summary>
+/// Order query service interface
+/// </summary>
+public interface IOrderQueryService
+{
+    /// <summary>
+    /// Handle get order by id query
+    /// </summary>
+    /// <param name="query"></param>
+    /// <returns>
+    /// The order if successful otherwise null
+    /// </returns>
+    Task<Orders?> Handle(GetOrderByIdQuery query);
+}
