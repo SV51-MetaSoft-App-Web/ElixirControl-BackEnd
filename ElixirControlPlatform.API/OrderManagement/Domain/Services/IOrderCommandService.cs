e06ad047206687697da18c86e861b7511a721e55
vquijandria <u201822697@upc.edu.pe>
2024-10-30 22:25:55 -0500

    feat(IOrderCommandService): added the OrderCommandService interface for handling the CreateOrderCommand

---
+++ ElixirControlPlatform.API/OrderManagement/Domain/Services/IOrderCommandService.cs
@@ -0,0 +1,19 @@
+﻿using MetaSoftAppWeb.API.OrderManagement.Domain.Model.Commands;
+
+
+
+/// <summary>
+/// Order command service interface
+/// </summary>
+
+public interface IOrderCommandService
+{
+    /// <summary>
+    /// Handle create order command
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns>
+    /// The created order if successful otherwise null
+    /// </returns>
+    Task<Orders?> Handle(CreateOrderCommand command);
+}
