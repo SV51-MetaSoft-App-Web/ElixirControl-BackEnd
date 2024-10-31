b39f3b1801a30e8ea53ee8071db3338b17939140
vquijandria <u201822697@upc.edu.pe>
2024-10-30 22:22:44 -0500

    feat(IOrderRepository): created the order repository interface

---
+++ ElixirControlPlatform.API/OrderManagement/Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
+using ElixirControlPlatform.APII.Shared.Domain.Repositories;
+
+//namespace ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+namespace ElixirControlPlatform.API.OrderManagement.Domain.Repositories;
+public interface IOrderRepository : IBaseRepository<Order>
+{
+    
+}
