212745392b32647a9c622299902d1789a28a16c0
vquijandria <u201822697@upc.edu.pe>
2024-10-30 23:32:36 -0500

    refactor: edited the namespace on the get order by id query

--- ElixirControlPlatform.API/OrderManagement/Domain/Model/Queries/GetOrderByIdQuery.cs
+++ ElixirControlPlatform.API/OrderManagement/Domain/Model/Queries/GetOrderByIdQuery.cs
@@ -1,3 +1,3 @@
 ﻿namespace ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
-
+namespace ElixirControl Platform.API.OrderManagement.Domain.Model.Queries;
 public record GetOrderByIdQuery(int Id);
