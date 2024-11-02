14ccb3a9dfb49a2aef127de17dbad0fb8798d673
Janover Saldaña <u20201b510@upc.edu.pe>
2024-11-01 21:16:11 -0500

    feat(winemaking-process): Added REST resources and transforms to add batch clarification stage.

---
+++ ElixirControlPlatform.API/WinemakingProcess/Interfaces/REST/Resources/AddClarificationToBatchResource.cs
@@ -0,0 +1,10 @@
+﻿namespace ElixirControlPlatform.API.WinemakingProcess.Interfaces.REST.Resources;
+
+public record AddClarificationToBatchResource(
+    int BatchId, 
+    string ProductsUsed, 
+    string ClarificationMethod, 
+    string FiltrationDate, 
+    double ClarityLevel, 
+    string StartDate, 
+    string EndDate);
