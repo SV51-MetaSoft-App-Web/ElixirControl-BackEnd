d503c2ebf7746a58cb3a1739b63b384d00d61889
vquijandria <u201822697@upc.edu.pe>
2024-11-01 19:23:39 -0500

    feat(OrdersController): finished the Orders Controller

--- ElixirControlPlatform.API/OrderManagement/Interfaces/OrdersController.cs
+++ ElixirControlPlatform.API/OrderManagement/Interfaces/OrdersController.cs
@@ -1,6 +1,52 @@
-﻿namespace ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+﻿using System.Net.Mime;
+using ElixirControlPlatform.API.OrderManagement.Domain.Model.Queries;
+using ElixirControlPlatform.API.OrderManagement.Domain.Services;
+using ElixirControlPlatform.API.OrderManagement.Interfaces.REST.Resources;
+using ElixirControlPlatform.API.OrderManagement.Interfaces.REST.Transform;
+using ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
-public class OrdersController
+namespace ElixirControlPlatform.API.OrderManagement.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("Orders")]
+[SwaggerTag("Available order endpoints")]
+public class OrdersController(IOrderCommandService orderCommandService, IOrderQueryService orderQueryService) : ControllerBase
 {
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create order",
+        Description = "Create a new order",
+        OperationId = "CreateOrder"
+    )]
+    [SwaggerResponse(StatusCodes.Status201Created, "The order was created", typeof(OrderResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "The order could not be created")]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderResource resource)
+    {
+        var createOrderCommand = CreateOrderCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var result = await orderCommandService.Handle(createOrderCommand);
+        if (result is null) return BadRequest();
+        return CreatedAtAction(nameof(GetOrderById), new {id = result.Id},
+            OrderResourceFromEntityAssembler.ToResourceFromEntity(result));
+    }
     
+    [HttpGet("{id}")]
+    [SwaggerOperation(
+        Summary = "Get order by id",
+        Description = "Get an order by its id",
+        OperationId = "GetOrderById"
+    )]
+    [SwaggerResponse(StatusCodes.Status200OK, "The order was found", typeof(OrderResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "The order was not found")]
+    public async Task<ActionResult> GetOrderById( int id)
+    {
+        var getOrderByIdQuery = new GetOrderByIdQuery(id);
+        var result = await orderQueryService.Handle(getOrderByIdQuery);
+        if (result is null) return NotFound();
+        var resource = OrderResourceFromEntityAssembler.ToResourceFromEntity(result);
+        return Ok(resource);
+    }
 }
