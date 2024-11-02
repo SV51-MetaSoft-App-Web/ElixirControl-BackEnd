facbdebccf9afd82906db7efe2bde529774fa6b0
vquijandria <u201822697@upc.edu.pe>
2024-11-01 22:24:07 -0500

    feat(OrderCommandService): added the Order Command Service

--- ElixirControlPlatform.API/OrderManagement/Application/Internal/CommandServices/OrderCommandService.cs
+++ ElixirControlPlatform.API/OrderManagement/Application/Internal/CommandServices/OrderCommandService.cs
@@ -1,11 +1,26 @@
-﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
+﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Aggregate;
+using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
 using ElixirControlPlatform.API.OrderManagement.Domain.Repositories;
 using ElixirControlPlatform.API.OrderManagement.Domain.Services;
 using ElixirControlPlatform.API.Shared.Domain.Repositories;
+using System.Threading.Tasks;
 
 namespace ElixirControlPlatform.API.OrderManagement.Application.Internal.CommandServices;
 
-public class OrderCommandService
+/// <summary>
+/// Order command service
+/// </summary>
+public class OrderCommandService(IOrderRepository orderRepository, IUnitOfWOrk unitOfWork) : IOrderCommandService
 {
-    public async Task<Orders?> Handle()
+
+    
+    public async Task<Order?> Handle(CreateOrderSourceCommand command)
+    {
+        var order = new Order(command);
+        
+            await orderRepository.AddAsync(order);
+            await unitOfWork.CompleteAsync();
+
+        return order;
+    }
 }
