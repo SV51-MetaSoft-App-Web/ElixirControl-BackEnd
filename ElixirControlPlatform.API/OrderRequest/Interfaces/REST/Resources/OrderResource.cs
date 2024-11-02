f50146b41e411a4ddd906fb97ad8c82d06a7c25c
Racso24k <u20211g192@upc.edu.pe>
2024-11-02 02:55:50 -0500

    feat(order-request): Order Resource added.

---
+++ ElixirControlPlatform.API/OrderRequest/Interfaces/REST/Resources/OrderResource.cs
@@ -0,0 +1,19 @@
+namespace ElixirControlPlatform.API.OrderRequest.Interfaces.REST.Resources;
+
+public record OrderResource(
+    int Id,
+    int Quantity,
+    int Price,
+    string Status,
+    string OrderNumber,
+    string OrderDate,
+    string TransportCondition,
+    string PaymentMethod,
+    string ConsumerPhone,
+    string ProducerPhone,
+    string PaymentTerms,
+    string Date,
+    string DeliveryDate,
+    string Type
+    
+    );
