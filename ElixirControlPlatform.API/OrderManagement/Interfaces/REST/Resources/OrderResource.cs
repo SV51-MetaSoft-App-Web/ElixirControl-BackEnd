833aabb74616be8b41ff4bc9000b363389459ca9
vquijandria <u201822697@upc.edu.pe>
2024-11-01 22:16:23 -0500

    feat(OrderResource): added the OrderResource

--- ElixirControlPlatform.API/OrderManagement/Interfaces/REST/Resources/OrderResource.cs
+++ ElixirControlPlatform.API/OrderManagement/Interfaces/REST/Resources/OrderResource.cs
@@ -1,3 +1,4 @@
-﻿namespace ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+﻿namespace ElixirControlPlatform.API.OrderManagement.Interfaces.REST.Resources;
+
 
 public record OrderResource(int Id, string BusinessName, string RequestedDate, int Quantity, string Phone, string Status, string ContactName, string ProductName, string TransportCondition, string PaymentTerms, string Address, string Email, string Ruc, string WineType, string PaymentMethod, string DeliveryDate);
