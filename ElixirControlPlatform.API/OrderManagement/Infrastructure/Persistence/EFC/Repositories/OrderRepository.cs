2a7b688717d50e3a5c64cbac284d14628e3faebf
vquijandria <u201822697@upc.edu.pe>
2024-11-01 22:20:38 -0500

    feat(OrderRepository): added the Order Repository

--- ElixirControlPlatform.API/OrderManagement/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
+++ ElixirControlPlatform.API/OrderManagement/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
@@ -1,11 +1,11 @@
-﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
+﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Aggregate;
 using ElixirControlPlatform.API.OrderManagement.Domain.Repositories;
-using ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using ElixirControlPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 namespace ElixirControlPlatform.API.OrderManagement.Infrastructure.Persistence.EFC.Repositories;
 
-public class OrderRepository(AppDbContext context) : BaseRepository<Orders>(context), IOrderRepository
+public class OrderRepository(AppDbContext context) : BaseRepository<Order>(context), IOrderRepository
 {
     
 }
