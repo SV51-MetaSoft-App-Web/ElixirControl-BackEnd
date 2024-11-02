a31fe7b917be548ce329b605a835473bbdf22827
vquijandria <u201822697@upc.edu.pe>
2024-11-01 22:23:50 -0500

    feat(OrderQueryService): added the Order Query Service

---
+++ ElixirControlPlatform.API/OrderManagement/Application/Internal/QueryServices/OrderQueryService.cs
@@ -0,0 +1,20 @@
+﻿using ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
+using ElixirControlPlatform.API.OrderManagement.Domain.Model.Queries;
+using ElixirControlPlatform.API.OrderManagement.Domain.Repositories;
+using ElixirControlPlatform.API.OrderManagement.Domain.Services;
+using ElixirControlPlatform.API.OrderManagement.Domain.Model.Aggregate;
+
+namespace ElixirControlPlatform.API.OrderManagement.Application.Internal.QueryServices;
+
+public class OrderQueryService(IOrderRepository orderRepository) : IOrderQueryService
+{
+    public async Task<Order?> Handle(GetOrderByIdQuery query)
+    {
+        return await orderRepository.FindByIdAsync(query.Id);
+    }
+
+    public async Task<IEnumerable<Order>> Handle(GetAllOrdersQuery query)
+    {
+        return await orderRepository.ListAsync();
+    }
+}
