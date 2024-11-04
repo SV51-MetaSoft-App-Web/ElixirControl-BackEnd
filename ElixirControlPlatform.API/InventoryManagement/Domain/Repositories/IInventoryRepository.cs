83fd1d6fb751045e3f2a4db49d4852eb8fed198c
LuisVJ97 <u201717523@upc.edu.pe>
2024-11-03 22:15:56 -0500

    feat(inventory-management): Update "IInventoryRepository"

--- ElixirControlPlatform.API/InventoryManagement/Domain/Repositories/IInventoryRepository.cs
+++ ElixirControlPlatform.API/InventoryManagement/Domain/Repositories/IInventoryRepository.cs
@@ -1,7 +1,11 @@
 ﻿
+using ElixirControlPlatform.API.InventoryManagement.Domain.Model.Aggregate;
+using ElixirControlPlatform.API.Shared.Domain.Repositories;
+
 namespace ElixirControlPlatform.API.InventoryManagement.Domain.Repositories;
 
-public interface IInventoryRepository
+public interface IInventoryRepository : IBaseRepository<Inventory>
 {
- 
+    Task<Inventory?> GetByIdAsync(int id); 
+    Task<IEnumerable<Inventory>> GetAllAsync();
 }
