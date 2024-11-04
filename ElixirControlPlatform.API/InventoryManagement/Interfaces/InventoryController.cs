ba0acf03fb2fe5000c0048b23bbe4dd823f452d0
LuisVJ97 <u201717523@upc.edu.pe>
2024-11-03 22:19:55 -0500

    feat(inventory-management): Update "InventoryController"

--- ElixirControlPlatform.API/InventoryManagement/Interfaces/InventoryController.cs
+++ ElixirControlPlatform.API/InventoryManagement/Interfaces/InventoryController.cs
@@ -12,43 +12,30 @@ namespace ElixirControlPlatform.API.InventoryManagement.Interfaces;
 [Route("api/v1/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
 [SwaggerTag("Available Inventory Endpoints")]
-public class InventoryController : ControllerBase
+public class InventoryController(IInventoryQueryService inventoryQueryService, IInventoryCommandService inventoryCommandService) : ControllerBase
 {
-    private readonly IInventoryQueryService inventoryQueryService;
-    private readonly IInventoryCommandService inventoryCommandService;
-
-    public InventoryController(IInventoryQueryService inventoryQueryService, IInventoryCommandService inventoryCommandService)
-    {
-        this.inventoryQueryService = inventoryQueryService;
-        this.inventoryCommandService = inventoryCommandService;
-    }
-
-    
     [HttpGet("{inventoryId:int}")]
     [SwaggerOperation(
         Summary = "Get an Inventory by id",
         Description = "Get an Inventory by id",
         OperationId = "GetInventoryById"
     )]
-    [SwaggerResponse(StatusCodes.Status200OK, "The inventory was successfully retrieved", typeof(InventoryResource))]
-    [SwaggerResponse(StatusCodes.Status404NotFound, "The inventory was not found")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The Inventory was successfully retrieved", typeof(InventoryResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "The Inventory was not found")]
     public async Task<IActionResult> GetInventoryById(int inventoryId)
     {
         var getInventoryByIdQuery = new GetInventoryByIdQuery(inventoryId);
         var inventory = await inventoryQueryService.Handle(getInventoryByIdQuery);
-        
         if (inventory is null) return NotFound();
         
         var inventoryResource = InventoryResourceFromEntityAssembler.ToResourceFromEntity(inventory);
-        
         return Ok(inventoryResource);
     }
-    
     
     [HttpPost]
     [SwaggerOperation(
         Summary = "Create an Inventory",
-        Description = "Create an Inventory",
+        Description = "Create a new Inventory",
         OperationId = "CreateInventory"
     )]
     [SwaggerResponse(StatusCodes.Status201Created, "The Inventory was successfully created", typeof(InventoryResource))]
@@ -56,30 +43,27 @@ public class InventoryController : ControllerBase
     public async Task<IActionResult> CreateInventory([FromBody] CreateInventoryResource resource)
     {
         var createInventoryCommand = CreateInventoryCommandFromResourceAssembler.ToCommandFromResource(resource);
-    
         var inventory = await inventoryCommandService.Handle(createInventoryCommand);
-
+        
         if (inventory is null) return BadRequest();
-
+        
         var inventoryResource = InventoryResourceFromEntityAssembler.ToResourceFromEntity(inventory);
-    
+        
         return CreatedAtAction(nameof(GetInventoryById), new { inventoryId = inventory.Id }, inventoryResource);
     }
     
-    
-   [HttpGet]
-   [SwaggerOperation(
-       Summary = "Get all Inventories",
-       Description = "Get all Inventories",
-       OperationId = "GetAllInventories"
-   )]
-   [SwaggerResponse(StatusCodes.Status200OK, "The Inventories were successfully retrieved", typeof(IEnumerable<InventoryResource>))]
-   public async Task<IActionResult> GetAllInventories()
-   {
-       var getAllInventoriesQuery = new GetAllInventoriesQuery();
-       var inventories = await inventoryQueryService.Handle(getAllInventoriesQuery);
-       var inventoryResources = inventories.Select(InventoryResourceFromEntityAssembler.ToResourceFromEntity);
-       
-       return Ok(inventoryResources);
-   }
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all Inventories",
+        Description = "Get all Inventories",
+        OperationId = "GetAllInventories"
+    )]
+    [SwaggerResponse(StatusCodes.Status200OK, "The Inventories were successfully retrieved", typeof(IEnumerable<InventoryResource>))]
+    public async Task<IActionResult> GetAllInventories()
+    {
+        var getAllInventoriesQuery = new GetAllInventoriesQuery();
+        var inventories = await inventoryQueryService.Handle(getAllInventoriesQuery);
+        var inventoryResources = inventories.Select(InventoryResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(inventoryResources);
+    }
 }
