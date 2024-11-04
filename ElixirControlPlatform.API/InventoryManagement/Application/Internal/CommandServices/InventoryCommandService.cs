7737c24a58039d6e9fe8e6f8ba83e116b033cb93
LuisVJ97 <u201717523@upc.edu.pe>
2024-11-03 22:11:20 -0500

    feat(inventory-management): Update "Inventory Command Service"

--- ElixirControlPlatform.API/InventoryManagement/Application/Internal/CommandServices/InventoryCommandService.cs
+++ ElixirControlPlatform.API/InventoryManagement/Application/Internal/CommandServices/InventoryCommandService.cs
@@ -1,23 +1,18 @@
 ﻿using ElixirControlPlatform.API.InventoryManagement.Domain.Model.Aggregate;
 using ElixirControlPlatform.API.InventoryManagement.Domain.Model.Commands;
+using ElixirControlPlatform.API.InventoryManagement.Domain.Repositories;
 using ElixirControlPlatform.API.InventoryManagement.Domain.Services;
 using ElixirControlPlatform.API.Shared.Domain.Repositories;
 
 
 namespace ElixirControlPlatform.API.InventoryManagement.Application.Internal.CommandServices;
 
-public class InventoryCommandService : IInventoryCommandService
+public class InventoryCommandService(IInventoryRepository inventoryRepository, IUnitOfWOrk unitOfWork) : IInventoryCommandService
 {
-    private readonly IUnitOfWOrk unitOfWork;
-
-    public InventoryCommandService(IUnitOfWOrk unitOfWork)
-    {
-        this.unitOfWork = unitOfWork;
-    }
-
     public async Task<Inventory?> Handle(CreateInventoryCommand command)
     {
-        var inventory = new Inventory(command);
+        var inventory = new Inventory(command); 
+        await inventoryRepository.AddAsync(inventory); 
         await unitOfWork.CompleteAsync();
         return inventory;
     }
