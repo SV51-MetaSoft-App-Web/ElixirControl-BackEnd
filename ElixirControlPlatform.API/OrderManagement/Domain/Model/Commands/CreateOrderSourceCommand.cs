af95f433aff62cac08029e871f59bcbeafdd4a1e
vquijandria <u201822697@upc.edu.pe>
2024-11-05 00:54:47 -0500

    refactor(CreateOrderCommand): renamed the command for creating the order

--- ElixirControlPlatform.API/OrderManagement/Domain/Model/Commands/CreateOrderSourceCommand.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace ElixirControlPlatform.API.OrderManagement.Domain.Model.Commands;
-
-public record CreateOrderSourceCommand(string BusinessName, string RequestedDate, int Quantity, string Phone, string Status, string ContactName, string ProductName, string TransportCondition, string PaymentTerms, string Address, string Email, string Ruc, string WineType, string PaymentMethod, string DeliveryDate);
