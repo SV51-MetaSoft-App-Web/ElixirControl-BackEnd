5164f0479b7bb478fa69a16f1db53ad4c281a7ed
petitavo <gustavohuanca1020@gmail.com>
2024-11-06 15:11:13 -0500

    feat(customer-management): delete and update client implemented.

--- ElixirControlPlatform.API/CustomerManagement/Application/Internal/CommandServices/ClientCommandService.cs
+++ ElixirControlPlatform.API/CustomerManagement/Application/Internal/CommandServices/ClientCommandService.cs
@@ -15,18 +15,36 @@ namespace ElixirControlPlatform.API.CustomerManagement.Application.Internal.Comm
 public class ClientCommandService(IClientRepository clientRepository, IUnitOfWOrk unitOfWork) : IClientCommandService
 {
     /// <inheritdoc cref="IClientCommandService.Handle"/>
-    public async Task<Client?> Handle(CreateClientsSourceCommand command)
+    public async Task<Client?> Handle(CreateClientCommand command)
     {
-        var client =
-            await clientRepository.FindByDniAsync(command.Dni);
-        if (client != null)
-            throw new Exception("Client with DNI already exists");
+        var client = await clientRepository.FindByDniAsync(command.Dni);
+        if (client != null) throw new Exception("Client with DNI already exists");
         client = new Client(command);
         await clientRepository.AddAsync(client);
         await unitOfWork.CompleteAsync();
         return client;
+        
+        
+    }
+
+    public async Task<Client?> Handle(DeleteClientByIdCommand command)
+    {
+        var client = await clientRepository.FindByIdAsync(command.Id);
+        if (client is null) throw new Exception("Client not found");
+        clientRepository.Remove(client);
+        await unitOfWork.CompleteAsync();
+        return client;
     }
     
-    
-    
+    public async Task<Client?> Handle(UpdateClientByIdCommand command)
+    {
+        var client = await clientRepository.FindByIdAsync(command.Id);
+        if (client is null) throw new Exception("Client not found");
+        var client2 = await clientRepository.FindByDniAsync(command.Dni);
+        if (client2 != null && client2.Id != command.Id) throw new Exception("Client with DNI already exists");
+        client.UpdateClientById(command);
+        clientRepository.Update(client);
+        await unitOfWork.CompleteAsync();
+        return client;
+    }
 }
