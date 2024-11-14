0df6739f9d1de2d6dda55d0228d0e0a83b6c6869
Janover Saldaña <u20201b510@upc.edu.pe>
2024-11-14 12:36:37 -0500

    feat(profile): Added interfaces for Commands, Queries, and Repository.

---
+++ ElixirControlPlatform.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using ElixirControlPlatform.API.Profiles.Domain.Model.Aggregate;
+using ElixirControlPlatform.API.Profiles.Domain.Model.ValueObjects;
+using ElixirControlPlatform.API.Shared.Domain.Repositories;
+
+namespace ElixirControlPlatform.API.Profiles.Domain.Repositories;
+
+public interface IProfileRepository : IBaseRepository<Profile>
+{
+    Task<Profile?> FindProfileByEmailAsync(EmailAddress email);
+    
+    Task<Profile?> GetProfileByProfileIdAsync(string profileId);
+    
+    Task<bool?> existsProfileByEmailAsync(string email);
+    
+    Task<bool?> existsProfileByProfileIdAsync(string profileId);
+    
+    Task<bool?> ExistsProfileByRUCAsync(string ruc);
+    
+}
---
+++ ElixirControlPlatform.API/Profiles/Domain/Services/IProfileQueryService.cs
@@ -0,0 +1,44 @@
+﻿using ElixirControlPlatform.API.Profiles.Domain.Model.Aggregate;
+using ElixirControlPlatform.API.Profiles.Domain.Model.Queries;
+
+namespace ElixirControlPlatform.API.Profiles.Domain.Services;
+
+/// <summary>
+/// Profile query service 
+/// </summary>
+public interface IProfileQueryService
+{
+    /// <summary>
+    /// Handle get all profiles 
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetAllProfilesQuery"/> query
+    /// </param>
+    /// <returns>
+    /// A list of <see cref="Profile"/> objects
+    /// </returns>
+    Task<IEnumerable<Profile>> Handle(GetAllProfilesQuery query);
+    
+    /// <summary>
+    /// Handle get profile by email 
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetProfileByEmailQuery"/> query
+    /// </param>
+    /// <returns>
+    /// A <see cref="Profile"/> object or null
+    /// </returns>
+    Task<Profile?> Handle(GetProfileByEmailQuery query);
+    
+    /// <summary>
+    /// Handle get profile by id 
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetProfileByProfileIdQuery"/> query
+    /// </param>
+    /// <returns><
+    /// A <see cref="Profile"/> object or null
+    /// /returns>
+    Task<Profile?> Handle(GetProfileByProfileIdQuery query);
+    
+}
