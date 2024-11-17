155e70ce8795cd2176f600ed70d3e3166094dcc6
Janover Saldaña <u20201b510@upc.edu.pe>
2024-11-17 04:14:05 -0500

    feat(winemaking-process): Added UpdateBatchResource

---
+++ ElixirControlPlatform.API/WinemakingProcess/Interfaces/REST/Resources/UpdateBatchResource.cs
@@ -0,0 +1,10 @@
+﻿namespace ElixirControlPlatform.API.WinemakingProcess.Interfaces.REST.Resources;
+
+public record UpdateBatchResource(
+    string VineyardCode,
+    string GrapeVariety,
+    string HarvestDate,
+    int GrapeQuantity,
+    string VineyardOrigin,
+    string ProcessStartDate
+);
